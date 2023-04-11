879ef8213126a4382b5e4d21656df69bbc1bbe28
sobunkohla <sobunkohla@gmail.com>
2023-04-11 09:24:58 +0200

    firebase-auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { auth } from "./firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({})
+  const [loading, setLoading] = useState(true)
+useEffect(() =>{
+onAuthStateChanged(auth, (user) => {
+  setLoading(false)
+  console.log(user);
+  if(user) {
+    setUser(user);
+  }
+})
+}, [])
+  function register() {
+    createUserWithEmailAndPassword(auth, "sobu@email.com", "test123")
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "sobu@email.com", "test123")
+      .then(({user}) => {
+        console.log(user);
+        setUser(user)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function logout() {
+  signOut(auth)
+  setUser({})
+  
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>login</button>
+      <button onClick={logout}>logout</button>
+      <h2>loading...</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
